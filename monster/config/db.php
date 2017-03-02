5544f0352601db03e42522d3dfc16c3a16d1a34d
Shinghai <hushinghai@gmail.com>
2017-03-02 07:00:47 +0000

    init

---
+++ monster/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=c9',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
